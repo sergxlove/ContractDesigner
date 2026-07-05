eba40ca4e5c2fa018131d3cd719cfc6be7217890
sergxlove <sergxlove@mail.ru>
2026-07-06 00:54:45 +0600

    add options, add text agreement

--- ContractDesigner.Web/Program.cs
+++ ContractDesigner.Web/Program.cs
@@ -1,3 +1,6 @@
+using ContractDesigner.Core.Abstractions;
+using ContractDesigner.Core.Models;
+using ContractDesigner.Core.Services;
 using ContractDesigner.Web.Hubs;
 using System.Diagnostics;
 
@@ -9,11 +12,30 @@ namespace ContractDesigner.Web
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddSignalR();
+            builder.Services.AddScoped<IRentalApartamentAgreement, RentalApartamentAgreement>();
             builder.WebHost.UseUrls("http://localhost:5000");
             var app = builder.Build();
             app.MapHub<ExitHub>("/hub");
-            app.UseStaticFiles();
+            //app.UseStaticFiles();
             app.MapGet("/", () => "Hello World!");
+
+            app.MapGet("/api/contract/generate", async (IRentalApartamentAgreement service) =>
+            {
+                try
+                {
+                    RentalApartamentAgreementOptions options = new();
+                    var pdfBytes = service.Generate(options);
+                    return Results.File(
+                        fileContents: pdfBytes,
+                        contentType: "application/pdf",
+                        fileDownloadName: $"Договор_аренды_{DateTime.Now:yyyyMMdd_HHmmss}.pdf"
+                    );
+                }
+                catch (Exception ex)
+                {
+                    return Results.BadRequest(new { error = ex.Message });
+                }
+            });
 
             Task.Run(() =>
             {
