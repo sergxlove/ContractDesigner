fd1338cf65d6d9e74310e38b827e7c6842c04e2e
sergxlove <sergxlove@mail.ru>
2026-07-08 20:39:54 +0600

    add item 2, 3

---
+++ ContractDesigner.Core/Models/People.cs
@@ -0,0 +1,8 @@
+﻿namespace ContractDesigner.Core.Models
+{
+    public class People
+    {
+        public string FullName { get; set; } = "text text text";
+        public string PassportData { get; set; } = "series number";
+    }
+}
