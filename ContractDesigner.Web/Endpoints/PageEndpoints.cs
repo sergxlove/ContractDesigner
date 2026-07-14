f32c2422510b20d25b317f496c3bc06dead76030
sergxlove <sergxlove@mail.ru>
2026-07-14 18:36:47 +0600

    add frontend, fix format pdf

---
+++ ContractDesigner.Web/Endpoints/PageEndpoints.cs
@@ -0,0 +1,21 @@
+﻿namespace ContractDesigner.Web.Endpoints
+{
+    public static class PageEndpoints
+    {
+        public static IEndpointRouteBuilder MapPageEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/", async (HttpContext context) =>
+            {
+                try
+                {
+                    context.Response.ContentType = "text/html; charset=utf-8";
+                    await context.Response.SendFileAsync("wwwroot/index.html");
+                }
+                catch { }
+
+            });
+
+            return app;
+        }
+    }
+}
